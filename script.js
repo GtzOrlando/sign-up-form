771a3325476dd91ebfe6453397e06574211f0384
OrlandoGtz <gtz.orlando@gmail.com>
2022-07-02 19:00:59 -0700

    improve form UX design for better clarity

--- script.js
+++ script.js
@@ -1,23 +1,21 @@
 let password1 = document.getElementById("password");
 let password2 = document.getElementById("confirmPass");
 let submitButton = document.getElementById("submitButton");
+let errorMessage = document.getElementById("errorMessage");
 
 function validatePass() {
-    if (password1.value.length > 7 && password2.value.length > 7) {
-        if (password1.value === password2.value) {
-            submitButton.disabled = false;
-            password1.className = "correct";
-            password2.className = "correct";
+    if (password1.value === password2.value) {
+        password2.setCustomValidity("");
+        password1.className = "correct";
+        password2.className = "correct";
+        errorMessage.textContent = "";
         } else {
-            submitButton.disabled = true;
-            password1.className = "error";
-            password2.className = "error";            
-        }
-    } else {
+        password2.setCustomValidity("Confirm password");
         password1.className = "error";
-        password2.className = "error";
-    }
-}
+        password2.className = "error";  
+        errorMessage.textContent = "*Passwords do not match";          
+        }
+ }
 
 password1.onchange = validatePass;
 password2.onkeyup = validatePass;
